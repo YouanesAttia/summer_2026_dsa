52cef06781e29db93b52822126a62c8651a49c45
YouanesAttia <youanesatia6@gmail.com>
2026-06-20 23:04:49 +0300

    Lowerbound using binary search template II

--- week3/day2/binarySearch.cpp
+++ week3/day2/binarySearch.cpp
@@ -82,3 +82,28 @@ int searchRotatedArray(std::vector<int> &arr, int target)
         }
     }
 }
+
+int lowerBound(std::vector<int> &arr, int target)
+{
+    if (arr.empty())
+        return 0;
+
+    int left = 0;
+    int right = arr.size();
+
+    while (left < right)
+    {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] < target)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid;
+        }
+    }
+
+    return left;
+}
