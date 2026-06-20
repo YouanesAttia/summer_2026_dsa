34ae52a597a88a108797b6ddae2a1fd468d989fe
YouanesAttia <youanesatia6@gmail.com>
2026-06-20 23:07:58 +0300

    Upperbound using binary search template II

--- week3/day2/binarySearch.cpp
+++ week3/day2/binarySearch.cpp
@@ -107,3 +107,25 @@ int lowerBound(std::vector<int> &arr, int target)
 
     return left;
 }
+
+int upperBound(std::vector<int> &arr, int target)
+{
+    int left = 0;
+    int right = arr.size();
+
+    while (left < right)
+    {
+        int mid = left + (right - left) / 2;
+
+        if (arr[mid] <= target)
+        {
+            left = mid + 1;
+        }
+        else
+        {
+            right = mid;
+        }
+    }
+
+    return left;
+}
