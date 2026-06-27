9a9a86bf68bbc71c852f321569b3274a0a11e6c0
YouanesAttia <youanesatia6@gmail.com>
2026-06-27 16:39:22 +0300

    coinChange dynamic programming

--- week4/day2/dp_intro.cpp
+++ week4/day2/dp_intro.cpp
@@ -1,5 +1,6 @@
 #include <iostream>
-#include <stdexcept>
+#include <vector>
+#include <algorithm>
 
 int climbStairs(const int &n)
 {
@@ -9,4 +10,30 @@ int climbStairs(const int &n)
     for (int i = 3; i <= n; i++)
         arr[i] = arr[i - 1] + arr[i - 2];
     arr[n];
+}
+
+int coinChange(const std::vector<int> &coins, const int amount)
+{
+    int dp[amount + 1];
+    for (int i = 0; i <= amount; i++)
+    {
+        dp[i] = amount + 1;
+    }
+
+    dp[0] = 0;
+    for (int i = 1; i <= amount; i++)
+    {
+        for (int coin : coins)
+        {
+            if (i - coin >= 0)
+            {
+                dp[i] = std::min(dp[i], 1 + dp[i - coin]);
+            }
+        }
+    }
+
+    if (dp[amount] == amount + 1)
+        return -1;
+    else
+        return dp[amount];
 }
