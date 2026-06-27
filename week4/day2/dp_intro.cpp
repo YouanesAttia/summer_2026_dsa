72b61e0ab29d35479d57b903167bc1fd32f77a84
YouanesAttia <youanesatia6@gmail.com>
2026-06-27 14:56:21 +0300

    Climb stairs dynamic programming

---
+++ week4/day2/dp_intro.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include <stdexcept>
+
+int climbStairs(const int &n)
+{
+    int arr[n + 1];
+    arr[1] = 1;
+    arr[2] = 2;
+    for (int i = 3; i <= n; i++)
+        arr[i] = arr[i - 1] + arr[i - 2];
+    arr[n];
+}
