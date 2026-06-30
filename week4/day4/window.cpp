e54fc9b338034f5728a6366be3168e5ec066257b
Youanes <youanesatia6@gmail.com>
2026-06-30 15:50:03 +0300

    Borders and Environment Options

--- week4/day4/window.cpp
+++ week4/day4/window.cpp
@@ -4,7 +4,9 @@ using namespace std;
 int main(int argc, char **argv)
 {
     initscr();
-
+    // cbreak(); //ctrl + c will exit you out
+    // raw(); //doesn't exit you but prints the char
+    // noeche(); //similar to raw but it doesn't print
     int height, width, start_y, start_x;
     height = 10;
     width = 20;
@@ -12,10 +14,11 @@ int main(int argc, char **argv)
     WINDOW *win = newwin(height, width, start_y, start_x);
     refresh();
 
-    box(win, 0, 0);
+    char c = 'g' box(win, (int)c, 104);
     mvwprintw(win, 1, 1, "This is my box");
     wrefresh(win);
 
+    getch();
     getch();
     endwin();
 }
